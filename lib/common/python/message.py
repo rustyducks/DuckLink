a612517fc8df61358f994005556aaa0f3d0bc028
Fabien-B <fabien.bonneval@gmail.com>
2019-09-19 04:53:52 +0200

    Create project structure.

---
+++ lib/common/python/message.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+class DuckMsg:
+
+    def __init__(self, msg_id, msg_name)
+        self.id = msg_id
+        self.name = msg_name
+
+    def get_id(self):
+        return self.id
+
+    def get_name(self):
+        return self.name
+
